62abbf8c91b6f81499ae81c5e1d2509befe5047a
AlejandraVe <alejandra.vera.vero@gmail.com>
2024-01-15 12:20:54 +0100

    Changed the current time using the API

--- src/script.js
+++ src/script.js
@@ -6,13 +6,38 @@ function updateCityAndWeather(response) {
   let description = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+  let currentTime = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
   todaysWeather.innerHTML = Math.round(responseTemperature);
   countryElement.innerHTML = response.data.country;
-  description.innerHTML = toUpperCase(response.data.condition.description);
+  description.innerHTML = response.data.condition.description;
   humidity.innerHTML = response.data.temperature.humidity;
   wind.innerHTML = response.data.wind.speed;
+  currentTime.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
